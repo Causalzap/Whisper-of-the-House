7109fee6fcb8c9b1f5c51585b9770f3369874018
causalzap <causalzap@gmail.com>
2025-12-17 10:18:31 +0800

    Add ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,31 +1,34 @@
 // app/layout.tsx
-import type { Metadata } from 'next';
-import '../styles/globals.css';
+import type { Metadata } from "next";
+import Script from "next/script";
+import "../styles/globals.css";
 
 export const metadata: Metadata = {
   title: {
-    default: 'Whisper of the House - Fansite',
-    template: '%s | Whisper of the House',
+    default: "Whisper of the House - Fansite",
+    template: "%s | Whisper of the House",
   },
   description:
-    'Explore Beginner\'s Guide, Progression Guide, Top Tips & Tricks, Hidden Secrets Guide , Organize and Discover Secrets and the full story of Whisper of the House',
-
-    icons: {
-      icon: [
-        { url: '/favicon.ico', type: 'image/x-icon' },
-        // 可选：再补充 png 版本，兼容性更好
-        // { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-        // { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      ],
-    },
-    alternates: {
-      canonical: 'https://www.whisperofthehouse.com/guides',
-    },
+    "Explore Beginner's Guide, Progression Guide, Top Tips & Tricks, Hidden Secrets Guide , Organize and Discover Secrets and the full story of Whisper of the House",
+  icons: {
+    icon: [{ url: "/favicon.ico", type: "image/x-icon" }],
+  },
+  alternates: {
+    canonical: "https://www.whisperofthehouse.com/guides",
+  },
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9769427406770321"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
