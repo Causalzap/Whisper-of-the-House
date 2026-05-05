d72a67267399c8658b5ac8d9382d79113c91a466
causalzap <causalzap@gmail.com>
2026-05-05 18:25:42 +0800

    updaet

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body>
-        {children}
+        {children} 
 
         <GoogleAnalytics gaId="G-CKPEMMMTCR" />
       </body>
